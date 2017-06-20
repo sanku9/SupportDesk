8c6ab47ee07c0a661de2868d4260f8d055c32874
sanku9 <hisantosh123@gmail.com>
2017-06-20 17:12:56 +0530

    Create Email.java

---
+++ src/main/java/com/supportdesk/domain/Email.java
@@ -0,0 +1,68 @@
+package com.supportdesk.domain;
+
+public class Email {
+
+	private String mailType = null;
+	private String mailFrom = null;
+	private String mailTo = null;
+	private String subject = null;
+	private String cc = null;
+	private String bcc = null;
+	private String body = null;
+
+	public String getMailType() {
+		return mailType;
+	}
+
+	public void setMailType(String mailType) {
+		this.mailType = mailType;
+	}
+
+	public String getMailFrom() {
+		return mailFrom;
+	}
+
+	public void setMailFrom(String mailFrom) {
+		this.mailFrom = mailFrom;
+	}
+
+	public String getMailTo() {
+		return mailTo;
+	}
+
+	public void setMailTo(String mailTo) {
+		this.mailTo = mailTo;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getCc() {
+		return cc;
+	}
+
+	public void setCc(String cc) {
+		this.cc = cc;
+	}
+
+	public String getBcc() {
+		return bcc;
+	}
+
+	public void setBcc(String bcc) {
+		this.bcc = bcc;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+}
