5b30d43a83fcb70e93d03974dc7a9454ef2da667
sanku9 <hisantosh123@gmail.com>
2017-06-20 17:12:13 +0530

    Create ChangeRequestReport.java

---
+++ src/main/java/com/supportdesk/domain/ChangeRequestReport.java
@@ -0,0 +1,169 @@
+package com.supportdesk.domain;
+
+
+public class ChangeRequestReport {
+	
+	private String changeId;
+    private String status;
+    private String summary;
+    private String priority;
+    private String riskLevel;
+    private String scheduledStartDate;
+    private String actualStartDate;
+    private String scheduledEndDate;
+    private String actualEndDate;
+    private String requestedEndDate;
+    private String changeCoordinator;
+    private String changeManager;
+    private String completedDate;
+    private String changeCoordinatorGroup;
+    private String lastModifiedDate;
+    private String managerGroup;
+    private String productName;
+    private String requestId;
+    private String serviceLevel;
+    private String submitDate;
+    private String targetDate;
+    private String totalTimeSpent;
+    private String note;
+	public String getChangeId() {
+		return changeId;
+	}
+	public void setChangeId(String changeId) {
+		this.changeId = changeId;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getPriority() {
+		return priority;
+	}
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	public String getRiskLevel() {
+		return riskLevel;
+	}
+	public void setRiskLevel(String riskLevel) {
+		this.riskLevel = riskLevel;
+	}
+	public String getScheduledStartDate() {
+		return scheduledStartDate;
+	}
+	public void setScheduledStartDate(String scheduledStartDate) {
+		this.scheduledStartDate = scheduledStartDate;
+	}
+	public String getActualStartDate() {
+		return actualStartDate;
+	}
+	public void setActualStartDate(String actualStartDate) {
+		this.actualStartDate = actualStartDate;
+	}
+	public String getScheduledEndDate() {
+		return scheduledEndDate;
+	}
+	public void setScheduledEndDate(String scheduledEndDate) {
+		this.scheduledEndDate = scheduledEndDate;
+	}
+	public String getActualEndDate() {
+		return actualEndDate;
+	}
+	public void setActualEndDate(String actualEndDate) {
+		this.actualEndDate = actualEndDate;
+	}
+	public String getRequestedEndDate() {
+		return requestedEndDate;
+	}
+	public void setRequestedEndDate(String requestedEndDate) {
+		this.requestedEndDate = requestedEndDate;
+	}
+	public String getChangeCoordinator() {
+		return changeCoordinator;
+	}
+	public void setChangeCoordinator(String changeCoordinator) {
+		this.changeCoordinator = changeCoordinator;
+	}
+	public String getChangeManager() {
+		return changeManager;
+	}
+	public void setChangeManager(String changeManager) {
+		this.changeManager = changeManager;
+	}
+	public String getCompletedDate() {
+		return completedDate;
+	}
+	public void setCompletedDate(String completedDate) {
+		this.completedDate = completedDate;
+	}
+	public String getChangeCoordinatorGroup() {
+		return changeCoordinatorGroup;
+	}
+	public void setChangeCoordinatorGroup(String changeCoordinatorGroup) {
+		this.changeCoordinatorGroup = changeCoordinatorGroup;
+	}
+	public String getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+	public void setLastModifiedDate(String lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+	public String getManagerGroup() {
+		return managerGroup;
+	}
+	public void setManagerGroup(String managerGroup) {
+		this.managerGroup = managerGroup;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String applicationId) {
+		this.productName = applicationId;
+	}
+	public String getRequestId() {
+		return requestId;
+	}
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+	public String getServiceLevel() {
+		return serviceLevel;
+	}
+	public void setServiceLevel(String serviceLevel) {
+		this.serviceLevel = serviceLevel;
+	}
+	public String getSubmitDate() {
+		return submitDate;
+	}
+	public void setSubmitDate(String submitDate) {
+		this.submitDate = submitDate;
+	}
+	public String getTargetDate() {
+		return targetDate;
+	}
+	public void setTargetDate(String targetDate) {
+		this.targetDate = targetDate;
+	}
+	public String getTotalTimeSpent() {
+		return totalTimeSpent;
+	}
+	public void setTotalTimeSpent(String totalTimeSpent) {
+		this.totalTimeSpent = totalTimeSpent;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+    
+    
+}
