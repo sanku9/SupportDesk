60f1d83bbfc150a06c525701eccc8e05e84e2e7c
sanku9 <hisantosh123@gmail.com>
2017-06-20 17:12:38 +0530

    Create IncidentReport.java

---
+++ src/main/java/com/supportdesk/domain/IncidentReport.java
@@ -0,0 +1,368 @@
+package com.supportdesk.domain;
+
+
+public class IncidentReport {
+
+	private String assignee;
+	private String assignedGroup;
+	private String assignedSupportCompany;
+	private String assignedSupportOrganization;
+	private String closedDate;
+	private String effortDurationHour;
+	private String escalated;
+	private String firstName;
+	private String goalCategoryChar;
+	private String groupTransfers;
+	private String incidentID;
+	private String individualTransfers;
+	private String lastDateDurationCalculated;
+	private String lastModifiedDate;
+	private String lastResolvedDate;
+	private String measurementStatus;
+	private String oLAHold;
+	private String overallStartTime;
+	private String overallStopTime;
+	private String ownerGroup;
+	private String ownerSupportCompany;
+	private String ownerSupportOrganization;
+	private String priority;
+	private String productName;
+	private String reOpenedDate;
+	private String reasonCode;
+	private String reasonDescription;
+	private String reportedDate;
+	private String resolution;
+	private String respondedDate;
+	private String sLAHold;
+	private String sLMPriority;
+	private String sLMRealTimeStatus;
+	private String sVTDueDate;
+	private String serviceRequestID;
+	private String status;
+	private String submitDate;
+	private String summary;
+	private String ticketType;
+	private String upElapsedTime;
+
+	public String getAssignee() {
+		return assignee;
+	}
+
+	public void setAssignee(String assignee) {
+		this.assignee = assignee;
+	}
+
+	public String getAssignedGroup() {
+		return assignedGroup;
+	}
+
+	public void setAssignedGroup(String assignedGroup) {
+		this.assignedGroup = assignedGroup;
+	}
+
+	public String getAssignedSupportCompany() {
+		return assignedSupportCompany;
+	}
+
+	public void setAssignedSupportCompany(String assignedSupportCompany) {
+		this.assignedSupportCompany = assignedSupportCompany;
+	}
+
+	public String getAssignedSupportOrganization() {
+		return assignedSupportOrganization;
+	}
+
+	public void setAssignedSupportOrganization(
+			String assignedSupportOrganization) {
+		this.assignedSupportOrganization = assignedSupportOrganization;
+	}
+
+	public String getClosedDate() {
+		return closedDate;
+	}
+
+	public void setClosedDate(String closedDate) {
+		this.closedDate = closedDate;
+	}
+
+	public String getEffortDurationHour() {
+		return effortDurationHour;
+	}
+
+	public void setEffortDurationHour(String effortDurationHour) {
+		this.effortDurationHour = effortDurationHour;
+	}
+
+	public String getEscalated() {
+		return escalated;
+	}
+
+	public void setEscalated(String escalated) {
+		this.escalated = escalated;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getGoalCategoryChar() {
+		return goalCategoryChar;
+	}
+
+	public void setGoalCategoryChar(String goalCategoryChar) {
+		this.goalCategoryChar = goalCategoryChar;
+	}
+
+	public String getGroupTransfers() {
+		return groupTransfers;
+	}
+
+	public void setGroupTransfers(String groupTransfers) {
+		this.groupTransfers = groupTransfers;
+	}
+
+	public String getIncidentID() {
+		return incidentID;
+	}
+
+	public void setIncidentID(String incidentID) {
+		this.incidentID = incidentID;
+	}
+
+	public String getIndividualTransfers() {
+		return individualTransfers;
+	}
+
+	public void setIndividualTransfers(String individualTransfers) {
+		this.individualTransfers = individualTransfers;
+	}
+
+	public String getLastDateDurationCalculated() {
+		return lastDateDurationCalculated;
+	}
+
+	public void setLastDateDurationCalculated(String lastDateDurationCalculated) {
+		this.lastDateDurationCalculated = lastDateDurationCalculated;
+	}
+
+	public String getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(String lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getLastResolvedDate() {
+		return lastResolvedDate;
+	}
+
+	public void setLastResolvedDate(String lastResolvedDate) {
+		this.lastResolvedDate = lastResolvedDate;
+	}
+
+	public String getMeasurementStatus() {
+		return measurementStatus;
+	}
+
+	public void setMeasurementStatus(String measurementStatus) {
+		this.measurementStatus = measurementStatus;
+	}
+
+	public String getoLAHold() {
+		return oLAHold;
+	}
+
+	public void setoLAHold(String oLAHold) {
+		this.oLAHold = oLAHold;
+	}
+
+	public String getOverallStartTime() {
+		return overallStartTime;
+	}
+
+	public void setOverallStartTime(String overallStartTime) {
+		this.overallStartTime = overallStartTime;
+	}
+
+	public String getOverallStopTime() {
+		return overallStopTime;
+	}
+
+	public void setOverallStopTime(String overallStopTime) {
+		this.overallStopTime = overallStopTime;
+	}
+
+	public String getOwnerGroup() {
+		return ownerGroup;
+	}
+
+	public void setOwnerGroup(String ownerGroup) {
+		this.ownerGroup = ownerGroup;
+	}
+
+	public String getOwnerSupportCompany() {
+		return ownerSupportCompany;
+	}
+
+	public void setOwnerSupportCompany(String ownerSupportCompany) {
+		this.ownerSupportCompany = ownerSupportCompany;
+	}
+
+	public String getOwnerSupportOrganization() {
+		return ownerSupportOrganization;
+	}
+
+	public void setOwnerSupportOrganization(String ownerSupportOrganization) {
+		this.ownerSupportOrganization = ownerSupportOrganization;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getReOpenedDate() {
+		return reOpenedDate;
+	}
+
+	public void setReOpenedDate(String reOpenedDate) {
+		this.reOpenedDate = reOpenedDate;
+	}
+
+	public String getReasonCode() {
+		return reasonCode;
+	}
+
+	public void setReasonCode(String reasonCode) {
+		this.reasonCode = reasonCode;
+	}
+
+	public String getReasonDescription() {
+		return reasonDescription;
+	}
+
+	public void setReasonDescription(String reasonDescription) {
+		this.reasonDescription = reasonDescription;
+	}
+
+	public String getReportedDate() {
+		return reportedDate;
+	}
+
+	public void setReportedDate(String reportedDate) {
+		this.reportedDate = reportedDate;
+	}
+
+	public String getResolution() {
+		return resolution;
+	}
+
+	public void setResolution(String resolution) {
+		this.resolution = resolution;
+	}
+
+	public String getRespondedDate() {
+		return respondedDate;
+	}
+
+	public void setRespondedDate(String respondedDate) {
+		this.respondedDate = respondedDate;
+	}
+
+	public String getsLAHold() {
+		return sLAHold;
+	}
+
+	public void setsLAHold(String sLAHold) {
+		this.sLAHold = sLAHold;
+	}
+
+	public String getsLMPriority() {
+		return sLMPriority;
+	}
+
+	public void setsLMPriority(String sLMPriority) {
+		this.sLMPriority = sLMPriority;
+	}
+
+	public String getsLMRealTimeStatus() {
+		return sLMRealTimeStatus;
+	}
+
+	public void setsLMRealTimeStatus(String sLMRealTimeStatus) {
+		this.sLMRealTimeStatus = sLMRealTimeStatus;
+	}
+
+	public String getsVTDueDate() {
+		return sVTDueDate;
+	}
+
+	public void setsVTDueDate(String sVTDueDate) {
+		this.sVTDueDate = sVTDueDate;
+	}
+
+	public String getServiceRequestID() {
+		return serviceRequestID;
+	}
+
+	public void setServiceRequestID(String serviceRequestID) {
+		this.serviceRequestID = serviceRequestID;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getSubmitDate() {
+		return submitDate;
+	}
+
+	public void setSubmitDate(String submitDate) {
+		this.submitDate = submitDate;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public String getTicketType() {
+		return ticketType;
+	}
+
+	public void setTicketType(String ticketType) {
+		this.ticketType = ticketType;
+	}
+
+	public String getUpElapsedTime() {
+		return upElapsedTime;
+	}
+
+	public void setUpElapsedTime(String upElapsedTime) {
+		this.upElapsedTime = upElapsedTime;
+	}
+
+}
