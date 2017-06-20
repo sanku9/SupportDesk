83ae7cd1fa96df6f6a5621c3025ff750c09ca095
sanku9 <hisantosh123@gmail.com>
2017-06-20 17:13:45 +0530

    Create SnowChangeRequestReport.java

---
+++ src/main/java/com/supportdesk/domain/SnowChangeRequestReport.java
@@ -0,0 +1,127 @@
+package com.supportdesk.domain;
+
+public class SnowChangeRequestReport {
+
+	private String Number;
+	private String Summary;
+	private String Priority;
+	private String AssignedTo;
+	private String Created;
+	private String Closed;
+	private String DueDate;
+	private String AssignmentGroup;
+	private String Status;
+	private String SubStatus;
+	private String Tasktype;
+	private String Description;
+	private String GroupHopcount;
+	private String AssignToHopCount;
+	private String CreatedBy;
+	private String Updated;
+	private String Updatedby;
+	public String getNumber() {
+		return Number;
+	}
+	public void setNumber(String number) {
+		Number = number;
+	}
+	public String getSummary() {
+		return Summary;
+	}
+	public void setSummary(String summary) {
+		Summary = summary;
+	}
+	public String getPriority() {
+		return Priority;
+	}
+	public void setPriority(String priority) {
+		Priority = priority;
+	}
+	public String getAssignedTo() {
+		return AssignedTo;
+	}
+	public void setAssignedTo(String assignedTo) {
+		AssignedTo = assignedTo;
+	}
+	public String getCreated() {
+		return Created;
+	}
+	public void setCreated(String created) {
+		Created = created;
+	}
+	public String getClosed() {
+		return Closed;
+	}
+	public void setClosed(String closed) {
+		Closed = closed;
+	}
+	public String getDueDate() {
+		return DueDate;
+	}
+	public void setDueDate(String dueDate) {
+		DueDate = dueDate;
+	}
+	public String getAssignmentGroup() {
+		return AssignmentGroup;
+	}
+	public void setAssignmentGroup(String assignmentGroup) {
+		AssignmentGroup = assignmentGroup;
+	}
+	public String getStatus() {
+		return Status;
+	}
+	public void setStatus(String status) {
+		Status = status;
+	}
+	public String getSubStatus() {
+		return SubStatus;
+	}
+	public void setSubStatus(String subStatus) {
+		SubStatus = subStatus;
+	}
+	public String getTasktype() {
+		return Tasktype;
+	}
+	public void setTasktype(String tasktype) {
+		Tasktype = tasktype;
+	}
+	public String getDescription() {
+		return Description;
+	}
+	public void setDescription(String description) {
+		Description = description;
+	}
+	public String getGroupHopcount() {
+		return GroupHopcount;
+	}
+	public void setGroupHopcount(String groupHopcount) {
+		GroupHopcount = groupHopcount;
+	}
+	public String getAssignToHopCount() {
+		return AssignToHopCount;
+	}
+	public void setAssignToHopCount(String assignToHopCount) {
+		AssignToHopCount = assignToHopCount;
+	}
+	public String getCreatedBy() {
+		return CreatedBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		CreatedBy = createdBy;
+	}
+	public String getUpdated() {
+		return Updated;
+	}
+	public void setUpdated(String updated) {
+		Updated = updated;
+	}
+	public String getUpdatedby() {
+		return Updatedby;
+	}
+	public void setUpdatedby(String updatedby) {
+		Updatedby = updatedby;
+	}
+	
+	
+
+}
