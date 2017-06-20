92a2cee7c34299163f3cc435b5db33fd9cdea265
sanku9 <hisantosh123@gmail.com>
2017-06-20 17:14:45 +0530

    Create WorkOrderReport.java

---
+++ src/main/java/com/supportdesk/domain/WorkOrderReport.java
@@ -0,0 +1,222 @@
+package com.supportdesk.domain;
+
+public class WorkOrderReport {
+
+	private String 	workOrderID;
+	private String 	status;
+	private String 	summary;
+	private String 	productName;
+	private String 	submitDate;
+	private String 	completedDate;
+	private String 	lastModifiedDate;
+	private String 	nextTargetDate;
+	private String 	reassignCounter;
+	private String 	assigneeSupportGroupName;
+	private String 	assigneeSupportCompany;
+	private String 	assigneeSupportOrganization;
+	private String 	associatedRequestID;
+	private String 	requestAssignee;
+	private String 	requestID;
+	private String 	sLMStatus;
+	private String 	serviceLevel;
+	private String 	ticketType;
+	private String 	priority;
+	private String 	requestedByFirstName;
+	private String 	requestedByLastName;
+	private String 	breachReason;
+	private String 	alstomBusinessEntity;
+	private String 	detailedDescription;
+
+	public String getWorkOrderID() {
+		return workOrderID;
+	}
+
+	public void setWorkOrderID(String workOrderID) {
+		this.workOrderID = workOrderID;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getSubmitDate() {
+		return submitDate;
+	}
+
+	public void setSubmitDate(String submitDate) {
+		this.submitDate = submitDate;
+	}
+
+	public String getCompletedDate() {
+		return completedDate;
+	}
+
+	public void setCompletedDate(String completedDate) {
+		this.completedDate = completedDate;
+	}
+
+	public String getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(String lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getNextTargetDate() {
+		return nextTargetDate;
+	}
+
+	public void setNextTargetDate(String nextTargetDate) {
+		this.nextTargetDate = nextTargetDate;
+	}
+
+	public String getReassignCounter() {
+		return reassignCounter;
+	}
+
+	public void setReassignCounter(String reassignCounter) {
+		this.reassignCounter = reassignCounter;
+	}
+
+	public String getAssigneeSupportGroupName() {
+		return assigneeSupportGroupName;
+	}
+
+	public void setAssigneeSupportGroupName(String assigneeSupportGroupName) {
+		this.assigneeSupportGroupName = assigneeSupportGroupName;
+	}
+
+	public String getAssigneeSupportCompany() {
+		return assigneeSupportCompany;
+	}
+
+	public void setAssigneeSupportCompany(String assigneeSupportCompany) {
+		this.assigneeSupportCompany = assigneeSupportCompany;
+	}
+
+	public String getAssigneeSupportOrganization() {
+		return assigneeSupportOrganization;
+	}
+
+	public void setAssigneeSupportOrganization(
+			String assigneeSupportOrganization) {
+		this.assigneeSupportOrganization = assigneeSupportOrganization;
+	}
+
+	public String getAssociatedRequestID() {
+		return associatedRequestID;
+	}
+
+	public void setAssociatedRequestID(String associatedRequestID) {
+		this.associatedRequestID = associatedRequestID;
+	}
+
+	public String getRequestAssignee() {
+		return requestAssignee;
+	}
+
+	public void setRequestAssignee(String requestAssignee) {
+		this.requestAssignee = requestAssignee;
+	}
+
+	public String getRequestID() {
+		return requestID;
+	}
+
+	public void setRequestID(String requestID) {
+		this.requestID = requestID;
+	}
+
+	public String getsLMStatus() {
+		return sLMStatus;
+	}
+
+	public void setsLMStatus(String sLMStatus) {
+		this.sLMStatus = sLMStatus;
+	}
+
+	public String getServiceLevel() {
+		return serviceLevel;
+	}
+
+	public void setServiceLevel(String serviceLevel) {
+		this.serviceLevel = serviceLevel;
+	}
+
+	public String getTicketType() {
+		return ticketType;
+	}
+
+	public void setTicketType(String ticketType) {
+		this.ticketType = ticketType;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+
+	public String getRequestedByFirstName() {
+		return requestedByFirstName;
+	}
+
+	public void setRequestedByFirstName(String requestedByFirstName) {
+		this.requestedByFirstName = requestedByFirstName;
+	}
+
+	public String getRequestedByLastName() {
+		return requestedByLastName;
+	}
+
+	public void setRequestedByLastName(String requestedByLastName) {
+		this.requestedByLastName = requestedByLastName;
+	}
+
+	public String getBreachReason() {
+		return breachReason;
+	}
+
+	public void setBreachReason(String breachReason) {
+		this.breachReason = breachReason;
+	}
+
+	public String getAlstomBusinessEntity() {
+		return alstomBusinessEntity;
+	}
+
+	public void setAlstomBusinessEntity(String alstomBusinessEntity) {
+		this.alstomBusinessEntity = alstomBusinessEntity;
+	}
+
+	public String getDetailedDescription() {
+		return detailedDescription;
+	}
+
+	public void setDetailedDescription(String detailedDescription) {
+		this.detailedDescription = detailedDescription;
+	}
+}
