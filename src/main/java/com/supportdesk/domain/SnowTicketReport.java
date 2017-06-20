8974fe1196328a8271751ff03078c41a51a62ed4
sanku9 <hisantosh123@gmail.com>
2017-06-20 17:14:05 +0530

    Create SnowTicketReport.java

---
+++ src/main/java/com/supportdesk/domain/SnowTicketReport.java
@@ -0,0 +1,168 @@
+package com.supportdesk.domain;
+
+public class SnowTicketReport {
+
+	private String task;
+	private String summary;
+	private String assignedTo;
+	private String assignmentGroup;
+	private String hasBreached;
+	private String stage;
+	private String created;
+	private String dueDate;
+	private String endTime;
+	private String description;
+	private String sla;
+	private String subStatus;
+	private String status;
+	private String taskType;
+	private String priority;
+	private String updated;
+	private String updatedBy;
+	private String groupHopCount;
+	private String assignToHopCount;
+	private String slaType;
+	private String plannedEndTime;
+	private String affectedCI;
+	private String reOpenCount;
+	
+	public String getTask() {
+		return task;
+	}
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getAssignedTo() {
+		return assignedTo;
+	}
+	public void setAssignedTo(String assignedTo) {
+		this.assignedTo = assignedTo;
+	}
+	public String getAssignmentGroup() {
+		return assignmentGroup;
+	}
+	public void setAssignmentGroup(String assignmentGroup) {
+		this.assignmentGroup = assignmentGroup;
+	}
+	public String getHasBreached() {
+		return hasBreached;
+	}
+	public void setHasBreached(String hasBreached) {
+		this.hasBreached = hasBreached;
+	}
+	public String getStage() {
+		return stage;
+	}
+	public void setStage(String stage) {
+		this.stage = stage;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	public String getDueDate() {
+		return dueDate;
+	}
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getSla() {
+		return sla;
+	}
+	public void setSla(String sla) {
+		this.sla = sla;
+	}
+	public String getSubStatus() {
+		return subStatus;
+	}
+	public void setSubStatus(String subStatus) {
+		this.subStatus = subStatus;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getTaskType() {
+		return taskType;
+	}
+	public void setTaskType(String taskType) {
+		this.taskType = taskType;
+	}
+	public String getPriority() {
+		return priority;
+	}
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	public String getUpdated() {
+		return updated;
+	}
+	public void setUpdated(String updated) {
+		this.updated = updated;
+	}
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	public String getGroupHopCount() {
+		return groupHopCount;
+	}
+	public void setGroupHopCount(String groupHopCount) {
+		this.groupHopCount = groupHopCount;
+	}
+	public String getAssignToHopCount() {
+		return assignToHopCount;
+	}
+	public void setAssignToHopCount(String assignToHopCount) {
+		this.assignToHopCount = assignToHopCount;
+	}
+	public String getSlaType() {
+		return slaType;
+	}
+	public void setSlaType(String slaType) {
+		this.slaType = slaType;
+	}
+	public String getPlannedEndTime() {
+		return plannedEndTime;
+	}
+	public void setPlannedEndTime(String plannedEndTime) {
+		this.plannedEndTime = plannedEndTime;
+	}
+	public String getAffectedCI() {
+		return affectedCI;
+	}
+	public void setAffectedCI(String affectedCI) {
+		this.affectedCI = affectedCI;
+	}
+	public String getReOpenCount() {
+		return reOpenCount;
+	}
+	public void setReOpenCount(String reOpenCount) {
+		this.reOpenCount = reOpenCount;
+	}
+		
+}
